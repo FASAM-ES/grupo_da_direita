d9b1ffa1846b0c6a759718d119e995944ed846df
Raphael Rodrigues <redemptor@live.com>
2014-11-29 14:57:35 -0200

    Teste nome do usuario closes #4

--- ProjetoExemplo/src/main/java/br/fasam/projetoexemplo/entidades/Usuario.java
+++ ProjetoExemplo/src/main/java/br/fasam/projetoexemplo/entidades/Usuario.java
@@ -19,6 +19,12 @@ public class Usuario {
     String cpf;
     List<Artigo> artigos;
     
+    public Usuario(String nome,String email, String senha){
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+    
     public Artigo getArtigo(Integer i) {
         return artigos.get(i);
     }
