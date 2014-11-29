065062a9b0087b6d166787f54851fd3975254523
Aurélio <Aurlio@172.16.4.72>
2014-11-29 11:25:03 -0200

    Inclusão da classe Comentario

---
+++ ProjetoExemplo/src/main/java/br/fasam/projetoexemplo/entidades/Comentario.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.fasam.projetoexemplo.entidades;
+
+/**
+ *
+ * @author Aurélio
+ */
+public class Comentario {
+    String Titulo;
+
+    public String getTitulo() {
+        return Titulo;
+    }
+
+    public void setTitulo(String Titulo) {
+        this.Titulo = Titulo;
+    }
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public void setDescricao(String Descricao) {
+        this.Descricao = Descricao;
+    }
+    String Descricao;
+}
