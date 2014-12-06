96c5cc1303cad06e5733dd950d70f5e314ffb3a2
redemptor <redemptor@live.com>
2014-12-06 15:45:44 -0200

    Documentando classes

--- ProjetoExemplo/src/main/java/br/fasam/projetoexemplo/entidades/Tag.java
+++ ProjetoExemplo/src/main/java/br/fasam/projetoexemplo/entidades/Tag.java
@@ -18,12 +18,20 @@ public class Tag {
         this.descricao = descricao;
     }
             
-    
+    /**
+     * Obtem artigo na lista de artigos
+     * @param i - indice do artigo
+     * @return artigo
+     */
     public Artigo getArtigo(Integer i) {
         return artigos.get(i);
         
     }
     
+    /**
+     * Adiciona artigo a lista de artigos
+     * @param artigo - artigo a ser adicionado
+     */
     public void addArtigo(Artigo artigo){
         if (this.artigos==null){
             this.artigos=new ArrayList<Artigo>();
@@ -31,24 +39,44 @@ public class Tag {
         this.artigos.add(artigo);
     }
     
+    /**
+     * Remove artigo da lista de artigos
+     * @param artigo - artigo a ser removido
+     */
     public void remArtigo(Artigo artigo) {
         if (this.artigos !=null) {
             this.artigos.remove(artigo);
         }
     }
 
+    /**
+     * Obtem nome da TAG
+     * @return nome
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Atribui nome da TAG
+     * @param nome - nome a ser atribuido
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    /**
+     * Obtem descrição da TAG
+     * @return descrição
+     */
     public String getDescricao() {
         return descricao;
     }
 
+    /**
+     * Atribui descrição da TAG
+     * @param descricao - descrição a ser atribuida
+     */
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
